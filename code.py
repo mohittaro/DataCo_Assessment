43bc84ab1810d0b1c27e5912e269ca3596cf943d
mohittaro <127325431+mohittaro@users.noreply.github.com>
2023-07-12 01:49:23 +0530

    Added Source File.

---
+++ code.py
@@ -0,0 +1,63 @@
+﻿import boto3
+import csv
+from kafka import KafkaConsumer
+
+# Kafka consumer configuration
+consumer = KafkaConsumer('clickstream_topic',
+    bootstrap_servers='localhost:9092',
+    group_id='clickstream_group')
+
+# AWS S3 configuration
+s3_bucket_name = 's3-bucket-name'
+s3_file_key = 'clickstream_data.csv'
+
+# Iterate over Kafka messages and process the data
+for message in consumer:
+    # Extract relevant fields from the message
+    user_id = message.key
+    timestamp = message.timestamp
+    url = message.value['url']
+    country = message.value['country']
+    city = message.value['city']
+    user_agent = message.value['user_agent']
+
+    # Create a list of data to write in CSV format
+    data = [user_id, timestamp, url, country, city, user_agent]
+
+    # Store the extracted data in AWS S3
+    s3_client = boto3.client('s3')
+    csv_buffer = StringIO()
+    csv_writer = csv.writer(csv_buffer)
+    csv_writer.writerow(data)
+    s3_client.put_object(
+        Body=csv_buffer.getvalue(),
+        Bucket=s3_bucket_name,
+        Key=s3_file_key)
+
+    # Print a success message
+    print('Data stored in S3 successfully!')
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count, countDistinct, avg
+
+# Create a Spark session
+spark = SparkSession.builder.appName("ClickstreamAggregation").getOrCreate()
+
+# Read clickstream data from the data store (Amazon S3)
+clickstream_data = spark.read.format("csv")\
+    .options(header=True, inferSchema=True)\
+    .load("s3://s3_bucket/clickstream_data.csv")
+
+# Perform aggregation by URL and country
+aggregated_data = clickstream_data.groupBy("URL", "country")\
+    .agg(count("click_id").alias("num_clicks"),
+    countDistinct("user_id").alias("num_unique_users"),
+    avg("time_spent").alias("avg_time_spent"))
+
+# Show the aggregated data
+aggregated_data.show()
+
+# Write the aggregated data to Elasticsearch
+aggregated_data.write.format("org.elasticsearch.spark.sql")\
+    .options(**elasticsearch_options)\
+    .save("elasticsearch_index/elasticsearch_type")
